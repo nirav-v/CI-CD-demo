c42a1d78d720060794b8226e0ddd508a4761c2bb
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-01 13:28:01 -0800

    pass tests

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -12,7 +12,7 @@ export default function Character({
   return (
     <div>
       <img src={imgUrl} alt={name + 'image'} />
-      <h2>fail the test {name}</h2>
+      <h2>{name}</h2>
       <ul>
         <li>Date of Birth: {birth}</li>
         <li>Date of Death: {death} </li>
